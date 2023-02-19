1a605fc4c49edc1f82776dba003b58c09f1c9954
Katie O'Donnell <katiebird57@gmail.com>
2023-02-19 12:28:57 -0500

    error handling for all sign up fields, if all full user is taken to login page

--- app/src/main/java/com/example/closetlogin/SignUp.java
+++ app/src/main/java/com/example/closetlogin/SignUp.java
@@ -2,10 +2,13 @@ package com.example.closetlogin;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.material.textfield.TextInputEditText;
 import com.vishnusivadas.advanced_httpurlconnection.PutData;
@@ -28,32 +31,64 @@ public class SignUp extends AppCompatActivity {
         buttonSignUp = findViewById(R.id.buttonSignUp);
         textViewLogin = findViewById(R.id.loginText);
 
-        //Start ProgressBar first (Set visibility VISIBLE)
-        Handler handler = new Handler(/*Looper.getMainLooper()*/);
-        handler.post(new Runnable() {
-
+        buttonSignUp.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void run() {
-                //Starting Write and Read data with URL
-                //Creating array for parameters
-                String[] field = new String[4];
-                field[0] = "fullname";
-                field[1] = "username";
-                field[2] = "password";
-                field[3] = "email";
-                //Creating array for data
-                String[] data = new String[4];
-                data[0] = "";
-                data[1] = "";
-                PutData putData = new PutData("https://projects.vishnusivadas.com/AdvancedHttpURLConnection/putDataTest.php", "POST", field, data);
-                if (putData.startPut()) {
-                    if (putData.onComplete()) {
-                        String result = putData.getResult();
-                        //End ProgressBar (Set visibility to GONE)
-                    }
+            public void onClick(View view) {
+                //create strings & initialize by getting vals from text box
+                String fullname, username, password, email;
+                fullname = String.valueOf(textInputEditTextFullname.getText());
+                username = String.valueOf(textInputEditTextUsername.getText());
+                password = String.valueOf(textInputEditTextPassword.getText());
+                email = String.valueOf(textInputEditTextEmail.getText());
+
+                //check if text boxes are empty
+                if (!username.equals("") && !fullname.equals("") && !email.equals("") && !password.equals("")) {
+                    //Start ProgressBar first (Set visibility VISIBLE)
+                    Handler handler = new Handler(/*Looper.getMainLooper()*/);
+                    handler.post(new Runnable() {
+
+                        @Override
+                        public void run() {
+                            //Starting Write and Read data with URL
+                            //Creating array for parameters
+                            String[] field = new String[4];
+                            field[0] = "fullname";
+                            field[1] = "username";
+                            field[2] = "password";
+                            field[3] = "email";
+                            //Creating array for data
+                            String[] data = new String[4];
+                            data[0] = fullname;
+                            data[1] = username;
+                            data[2] = password;
+                            data[3] = email;
+                            PutData putData = new PutData("http://10.0.0.203/LoginRegister/signup.php", "POST", field, data);
+                            if (putData.startPut()) {
+                                if (putData.onComplete()) {
+                                    String result = putData.getResult();
+                                    if (result.equals("Sign Up Success")){
+                                        //opens login form if sign up successful
+                                        Toast.makeText(getApplicationContext(), result, Toast.LENGTH_SHORT).show();
+                                        Intent intent = new Intent(getApplicationContext(), Login.class);
+                                        startActivity(intent);
+                                        finish();
+                                    }
+                                    else{
+                                        Toast.makeText(getApplicationContext(), result, Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            }
+                            //End Write and Read data with URL
+                        }
+                    });
                 }
-                //End Write and Read data with URL
+                //error msg if not all fields are filled
+                else{
+                    Toast.makeText(getApplicationContext(), "All fields are required", Toast.LENGTH_SHORT).show();
+                }
             }
         });
+
+
     }
 }
