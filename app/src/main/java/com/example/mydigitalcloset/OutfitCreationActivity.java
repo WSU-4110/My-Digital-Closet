b199d8e15db89921bc69adc54e08eed062f12606
Katie O'Donnell <122855315+k-odonnell@users.noreply.github.com>
2023-02-21 16:20:22 -0500

    started onclick functionality in outfit creation page

--- app/src/main/java/com/example/mydigitalcloset/OutfitCreationActivity.java
+++ app/src/main/java/com/example/mydigitalcloset/OutfitCreationActivity.java
@@ -1,14 +1,42 @@
 package com.example.mydigitalcloset;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
+import com.example.mydigitalcloset.databinding.ActivityOutfitCreationBinding;
+
 public class OutfitCreationActivity extends AppCompatActivity {
+
+    ActivityOutfitCreationBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_outfit_creation);
+        binding = ActivityOutfitCreationBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+            switch(item.getItemId()){
+                case R.id.home:
+                    break;
+                case R.id.wardrobe:
+                    break;
+                case R.id.addItem:
+                    break;
+                case R.id.contactSupport:
+                    break;
+            }
+
+            return true;
+        });
+    }
+
+    private void replaceFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
     }
 }
