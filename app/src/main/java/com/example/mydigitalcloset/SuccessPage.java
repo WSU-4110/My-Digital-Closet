f5f5c131f3cfb93b27f5840d7c6b4230bd3e3cd1
RomanWSU <122497955+RomanWSU@users.noreply.github.com>
2023-02-21 03:51:40 -0500

    Contact Form

---
+++ app/src/main/java/com/example/mydigitalcloset/SuccessPage.java
@@ -0,0 +1,14 @@
+package com.example.mydigitalcloset;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SuccessPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_success_page);
+    }
+}
