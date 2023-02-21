e6f05ee0febff20525bba6e88f522e6a1cc736cb
Katie O'Donnell <122855315+k-odonnell@users.noreply.github.com>
2023-02-21 02:02:34 -0500

    Edited SignUp.java to process input data from text fields and save user data to the database. Set login page to be the start page of the app

--- app/src/main/java/com/example/mydigitalcloset/SignUp.java
+++ app/src/main/java/com/example/mydigitalcloset/SignUp.java
@@ -2,13 +2,92 @@ package com.example.mydigitalcloset;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.textfield.TextInputEditText;
+import com.vishnusivadas.advanced_httpurlconnection.PutData;
 
 public class SignUp extends AppCompatActivity {
+
+    //signup page elements
+    TextInputEditText textInputEditTextFullname, textInputEditTextEmail, textInputEditTextPassword, textInputEditTextUsername;
+    Button buttonSignUp;
+    TextView textViewLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        textInputEditTextFullname = findViewById(R.id.fullname);
+        textInputEditTextUsername = findViewById(R.id.username);
+        textInputEditTextPassword = findViewById(R.id.password);
+        textInputEditTextEmail = findViewById(R.id.email);
+        buttonSignUp = findViewById(R.id.buttonSignUp);
+        textViewLogin = findViewById(R.id.loginText);
+
+        buttonSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String fullname, username, password, email;
+                fullname = String.valueOf(textInputEditTextFullname.getText());
+                username = String.valueOf(textInputEditTextUsername.getText());
+                password = String.valueOf(textInputEditTextPassword.getText());
+                email = String.valueOf(textInputEditTextEmail.getText());
+
+                //make sure every field is entered
+                if(!fullname.equals("") && !username.equals("") && !password.equals("") && !email.equals("")) {
+
+                    Handler handler = new Handler();
+                    handler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            //Get data from database, starting Write and Read data with URL
+                            //Creating array for parameters
+                            String[] field = new String[4];
+                            field[0] = "fullname";
+                            field[1] = "username";
+                            field[3] = "password";
+                            field[4] = "email";
+                            //Creating array for data
+                            String[] data = new String[4];
+                            data[0] = "fullname";
+                            data[1] = "username";
+                            data[2] = "password";
+                            data[3] = "email";
+                            PutData putData = new PutData("http://10.0.0.203/LoginRegister/signup.php", "POST", field, data);
+                            if (putData.startPut()) {
+                                if (putData.onComplete()) {
+                                    String result = putData.getResult();
+                                    if (result.equals("Signup Success")){
+                                        //go to login page & show toast if signup is successful
+                                        Intent intent = new Intent(getApplicationContext(), Login.class);
+                                        startActivity(intent);
+                                        finish();
+                                    }
+                                    else{
+                                        //show unsuccessful result toast if signup unsuccessful
+                                        Toast.makeText(getApplicationContext(), result, Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            }
+                            //End Write and Read data with URL
+                        }
+                    });
+                }
+                //toast shown if any input areas are missing
+                else{
+                    Toast.makeText(getApplicationContext(), "All fields required", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
     }
 }
