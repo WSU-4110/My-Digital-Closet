786664173223ab55965124485eb7365d80e51a2b
Katie O'Donnell <122855315+k-odonnell@users.noreply.github.com>
2023-02-21 16:41:37 -0500

    added menu bar functionality

--- app/src/main/java/com/example/mydigitalcloset/OutfitCreationActivity.java
+++ app/src/main/java/com/example/mydigitalcloset/OutfitCreationActivity.java
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.example.mydigitalcloset.databinding.ActivityOutfitCreationBinding;
@@ -22,21 +23,37 @@ public class OutfitCreationActivity extends AppCompatActivity {
         binding.bottomNavigationView.setOnItemSelectedListener(item -> {
             switch(item.getItemId()){
                 case R.id.home:
+                    Intent intentHome = new Intent(getApplicationContext(), OutfitCreationActivity.class);
+                    startActivity(intentHome);
+                    finish();
                     break;
-                case R.id.wardrobe:
-                    break;
+                /*case R.id.wardrobe:
+                    Intent intentWardrobe = new Intent(getApplicationContext(), OutfitCreationActivity.class);
+                    startActivity(intentWardrobe);
+                    finish();
+                    break;*/
                 case R.id.addItem:
+                    Intent intentAdd = new Intent(getApplicationContext(), clothingFront.class);
+                    startActivity(intentAdd);
+                    finish();
                     break;
                 case R.id.contactSupport:
+                    Intent intentContact = new Intent(getApplicationContext(), ContactForm.class);
+                    startActivity(intentContact);
+                    finish();
                     break;
             }
 
             return true;
         });
+
+
     }
 
-    private void replaceFragment(Fragment fragment){
+    /*private void replaceFragment(Fragment fragment){
         FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-    }
+        FragmentTransaction.replace(R.id.frame_layout, fragment);
+        FragmentTransaction.commit();
+    }*/
 }
