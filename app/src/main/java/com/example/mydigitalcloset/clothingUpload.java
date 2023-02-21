6f752f52e4b8810f1e3f8af6976becf382d272b9
Nick Zuke <nicholasezuke@gmail.com>
2023-02-21 14:27:20 -0500

    Clothing page

---
+++ app/src/main/java/com/example/mydigitalcloset/clothingUpload.java
@@ -0,0 +1,14 @@
+package com.example.mydigitalcloset;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class clothingUpload extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_clothing_upload);
+    }
+}
