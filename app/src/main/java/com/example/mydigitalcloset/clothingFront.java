142fdba973352331075184ba011b86609ebbf6ad
Katie O'Donnell <122855315+k-odonnell@users.noreply.github.com>
2023-03-15 16:09:28 -0400

    modified nav bar functionality for clothing items page

--- app/src/main/java/com/example/mydigitalcloset/clothingFront.java
+++ app/src/main/java/com/example/mydigitalcloset/clothingFront.java
@@ -2,12 +2,16 @@ package com.example.mydigitalcloset;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.Button;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.mydigitalcloset.closetPage.AllSavedOufitsPage;
 import com.example.mydigitalcloset.databinding.ActivityClothingFrontBinding;
 import com.example.mydigitalcloset.databinding.ActivityOutfitCreationBinding;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class clothingFront extends AppCompatActivity {
 
@@ -20,32 +24,35 @@ public class clothingFront extends AppCompatActivity {
         //NAV BAR STUFF:
         binding = ActivityClothingFrontBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-
-        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
-            switch(item.getItemId()){
-                case R.id.home:
-                    Intent intentHome = new Intent(getApplicationContext(), OutfitCreationActivity.class);
-                    startActivity(intentHome);
-                    finish();
-                    break;
-                /*case R.id.wardrobe:
-                    Intent intentWardrobe = new Intent(getApplicationContext(), OutfitCreationActivity.class);
-                    startActivity(intentWardrobe);
-                    finish();
-                    break;*/
-                case R.id.addItem:
-                    Intent intentAdd = new Intent(getApplicationContext(), clothingFront.class);
-                    startActivity(intentAdd);
-                    finish();
-                    break;
-                case R.id.contactSupport:
-                    Intent intentContact = new Intent(getApplicationContext(), ContactForm.class);
-                    startActivity(intentContact);
-                    finish();
-                    break;
+        binding.bottomNavigationView.setOnItemSelectedListener(new BottomNavigationView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.home:
+                        startActivity(new Intent(clothingFront.this, OutfitCreationActivity.class));
+                        break;
+                    case R.id.addItem:
+                        /*Intent intentAdd = new Intent(getApplicationContext(), clothingFront.class);
+                        startActivity(intentAdd);*/
+                        //startActivity(new Intent(clothingFront.this, clothingFront.class));
+                        //finish();
+                        return true;
+                        //break;
+                    case R.id.contactSupport:
+                        /*Intent intentContact = new Intent(getApplicationContext(), ContactForm.class);
+                        startActivity(intentContact);*/
+                        startActivity(new Intent(clothingFront.this, ContactForm.class));
+                        //finish();
+                        break;
+                    case R.id.wardrobe:
+                        startActivity(new Intent(clothingFront.this, AllSavedOufitsPage.class));
+                        break;
+                    /*case R.id.settings:
+                        startActivity(new Intent(OutfitCreationActivity.this, SETTINGS.class));
+                        break;*/
+                }
+                return true;
             }
-
-            return true;
         });
         //end nav bar
 
