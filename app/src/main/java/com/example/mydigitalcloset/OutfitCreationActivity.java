4fdfbc1d6b2fed677f96c7e3250bb947bd59b201
Katie O'Donnell <122855315+k-odonnell@users.noreply.github.com>
2023-03-17 18:52:53 -0400

    set up page to fetch top image from database

--- app/src/main/java/com/example/mydigitalcloset/OutfitCreationActivity.java
+++ app/src/main/java/com/example/mydigitalcloset/OutfitCreationActivity.java
@@ -3,23 +3,32 @@ package com.example.mydigitalcloset;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ProgressBar;
 
 import com.example.mydigitalcloset.closetPage.AllSavedOufitsPage;
 import com.example.mydigitalcloset.databinding.ActivityOutfitCreationBinding;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 import org.jetbrains.annotations.Nullable;
+
+import java.io.File;
+import java.io.IOException;
 
 public class OutfitCreationActivity extends AppCompatActivity {
 
     ActivityOutfitCreationBinding binding;
+    StorageReference storageReference;
+    ProgressDialog progressDialog;
     FirebaseAuth mAuth;
     Context context;
 
@@ -31,9 +40,11 @@ public class OutfitCreationActivity extends AppCompatActivity {
         // Initialize Firebase Auth
         mAuth = FirebaseAuth.getInstance();
 
-        // Inflate the layout and set up the bottom navigation view
+        // Inflate the layout
         binding = ActivityOutfitCreationBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        //nav bar:
         binding.bottomNavigationView.setOnItemSelectedListener(new BottomNavigationView.OnItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
@@ -61,6 +72,31 @@ public class OutfitCreationActivity extends AppCompatActivity {
                         break;*/
                 }
                 return true;
+            }
+        });
+        //end nav bar
+
+        //get top image from firebase:
+        binding.getTop.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                progressDialog = new ProgressDialog(OutfitCreationActivity.this);
+                progressDialog.setMessage("Fetching top image...");
+                progressDialog.setCancelable(false);
+                progressDialog.show();
+
+                //use top name entered by user to get top from database
+                String topID = binding.getTopName.getText().toString();
+                storageReference = FirebaseStorage.getInstance().getReference("images/tops/"+topID+".png");
+
+                //create local file for top image
+                try{
+                    File topfile = File.createTempFile("tempfile_top", ".png");
+                } catch(IOException e){
+                    e.printStackTrace();
+                }
+
+
             }
         });
     }
