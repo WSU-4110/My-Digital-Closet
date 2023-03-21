6dc980f366c7fe25892e1fbc70ff5b88cd972cdc
Nick Zuke <nicholasezuke@gmail.com>
2023-03-21 15:03:21 -0400

    Attempted to get firebase to load up an image to less than stellar (as in, no) results

--- app/src/main/java/com/example/mydigitalcloset/clothingFront.java
+++ app/src/main/java/com/example/mydigitalcloset/clothingFront.java
@@ -1,9 +1,11 @@
 package com.example.mydigitalcloset;
 
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 
 import androidx.annotation.NonNull;
@@ -13,17 +15,20 @@ import com.example.mydigitalcloset.closetPage.AllSavedOufitsPage;
 import com.example.mydigitalcloset.databinding.ActivityClothingFrontBinding;
 //import com.example.mydigitalcloset.databinding.ActivityOutfitCreationBinding;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 public class clothingFront extends AppCompatActivity {
 
     ActivityClothingFrontBinding binding;
     Context context;
-
+    ProgressDialog progressDialog;
+    StorageReference storageReference;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         context = this;
         super.onCreate(savedInstanceState);
-
+        setContentView(R.layout.activity_clothing_front);
         //NAV BAR STUFF:
         binding = ActivityClothingFrontBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
@@ -57,12 +62,17 @@ public class clothingFront extends AppCompatActivity {
         });
         //end nav bar
 
-        setContentView(R.layout.activity_clothing_front);
-        //this moves to the page that adds clothing items
+        //buttons!
+
+        //go to clothing upload page
         Button addPage = findViewById(R.id.addItem);
         addPage.setOnClickListener(view -> {
             Intent intent=new Intent(clothingFront.this,clothingUpload.class);
             startActivity(intent);
         });
-    }
+
+
+
+
+    }//end of on create
 }
--- app/src/main/java/com/example/mydigitalcloset/clothingUpload.java
+++ app/src/main/java/com/example/mydigitalcloset/clothingUpload.java
@@ -1,16 +1,24 @@
 package com.example.mydigitalcloset;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.mydigitalcloset.databinding.ActivityClothingUploadBinding;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 public class clothingUpload extends AppCompatActivity {
     private DatabaseReference mDatabase;
-
+    ProgressDialog progressDialog;
+    ActivityClothingUploadBinding binding;
+    StorageReference storageReference;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,7 +27,7 @@ public class clothingUpload extends AppCompatActivity {
         // Get a reference to the "clothing" node in your Firebase Realtime Database
         mDatabase = FirebaseDatabase.getInstance().getReference().child("clothing");
 
-        Button topsButton = findViewById(R.id.button);
+        Button topsButton = findViewById(R.id.addTops);
         topsButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -29,7 +37,7 @@ public class clothingUpload extends AppCompatActivity {
             }
         });
 
-        Button bottomsButton = findViewById(R.id.button3);
+        Button bottomsButton = findViewById(R.id.addBottoms);
         bottomsButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -39,7 +47,7 @@ public class clothingUpload extends AppCompatActivity {
             }
         });
 
-        Button shoesButton = findViewById(R.id.button4);
+        Button shoesButton = findViewById(R.id.addShoes);
         shoesButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -49,7 +57,7 @@ public class clothingUpload extends AppCompatActivity {
             }
         });
 
-        Button socksButton = findViewById(R.id.button6);
+        Button socksButton = findViewById(R.id.addSocks);
         socksButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -59,7 +67,7 @@ public class clothingUpload extends AppCompatActivity {
             }
         });
 
-        Button headwearButton = findViewById(R.id.button5);
+        Button headwearButton = findViewById(R.id.addHeadwear);
         headwearButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -69,7 +77,7 @@ public class clothingUpload extends AppCompatActivity {
             }
         });
 
-        Button otherButton = findViewById(R.id.button7);
+        Button otherButton = findViewById(R.id.addOther);
         otherButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -78,5 +86,35 @@ public class clothingUpload extends AppCompatActivity {
                 mDatabase.child("other").child(clothingId).setValue("new item");
             }
         });
+        //buttons!
+
+        //Back to clothing front page
+        Button b2c = findViewById(R.id.backToClothUp);
+        b2c.setOnClickListener(view -> {
+            Intent intent=new Intent(clothingUpload.this,clothingFront.class);
+            startActivity(intent);
+        });
+
+        Button topPage = findViewById(R.id.addTops);
+        topPage.setOnClickListener(view -> {
+            Intent intent=new Intent(clothingUpload.this,clothingFront.class);
+            startActivity(intent);
+        });
+
+        /*binding.addTops.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                progressDialog = new ProgressDialog(clothingUpload.this);
+                progressDialog.setMessage("Test...");
+                progressDialog.setCancelable(false);
+                progressDialog.show();
+
+                //String imageID = binding.etimageId.getText().toString();
+
+                //storageReference = FirebaseStorage.getInstance().getReference
+            }
+        });*/
+
     }
+
+
 }
