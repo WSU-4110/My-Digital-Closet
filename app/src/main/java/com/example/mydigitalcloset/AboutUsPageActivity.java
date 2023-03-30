70b6381783f551bdea008aa5d9e87bc4d9c77a8e
Katie O'Donnell <122855315+k-odonnell@users.noreply.github.com>
2023-03-30 17:13:17 -0400

    added nav buttons to about us page

--- app/src/main/java/com/example/mydigitalcloset/AboutUsPageActivity.java
+++ app/src/main/java/com/example/mydigitalcloset/AboutUsPageActivity.java
@@ -12,6 +12,7 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.mydigitalcloset.closetPage.AllSavedOufitsPage;
 import com.example.mydigitalcloset.databinding.ActivityAboutusPageBinding;
 import com.example.mydigitalcloset.databinding.ActivityOutfitCreationBinding;
 
@@ -23,6 +24,49 @@ public class AboutUsPageActivity extends AppCompatActivity {
       // Inflate the layout
       binding = ActivityAboutusPageBinding.inflate(getLayoutInflater());
       setContentView(binding.getRoot());
-    }
+
+      //navigation buttons
+      binding.homeButton.setOnClickListener(new View.OnClickListener() {
+          @Override
+          public void onClick(View view) {
+              Intent intent = new Intent(getApplicationContext(), OutfitCreationActivity.class);
+              startActivity(intent);
+              finish();
+          }
+      });
+      binding.wardrobeButton.setOnClickListener(new View.OnClickListener() {
+          @Override
+          public void onClick(View view) {
+              Intent intent = new Intent(getApplicationContext(), AllSavedOufitsPage.class);
+              startActivity(intent);
+              finish();
+          }
+      });
+      binding.addItemButton.setOnClickListener(new View.OnClickListener() {
+          @Override
+          public void onClick(View view) {
+              Intent intent = new Intent(getApplicationContext(), clothingFront.class);
+              startActivity(intent);
+              finish();
+          }
+      });
+      binding.contactButton.setOnClickListener(new View.OnClickListener() {
+          @Override
+          public void onClick(View view) {
+              Intent intent = new Intent(getApplicationContext(), AboutUsPageActivity.class); //change depending on romans pages
+              startActivity(intent);
+              finish();
+          }
+      });
+        /*binding.settingsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), ?.class);
+                startActivity(intent);
+                finish();
+            }
+        });*/
+      //end nav buttons
+    }//end onCreate
 
 }
