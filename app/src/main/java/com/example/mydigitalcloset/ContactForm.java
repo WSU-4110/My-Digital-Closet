185c9aa8bb2ebc83ad44bea8f30f13d90a0421f5
Katie O'Donnell <122855315+k-odonnell@users.noreply.github.com>
2023-03-30 17:07:31 -0400

    removed navbar functionality & added nav button functionality to clothing front

--- app/src/main/java/com/example/mydigitalcloset/ContactForm.java
+++ app/src/main/java/com/example/mydigitalcloset/ContactForm.java
@@ -30,62 +30,6 @@ public class ContactForm extends AppCompatActivity {
         binding = ActivityContactFormBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        /*binding.bottomNavigationView.setOnItemSelectedListener(item -> {
-            switch(item.getItemId()){
-                case R.id.home:
-                    Intent intentHome = new Intent(getApplicationContext(), HomeFragment.class);
-                    startActivity(intentHome);
-                    finish();
-                    break;
-                /*case R.id.wardrobe:
-                    Intent intentWardrobe = new Intent(getApplicationContext(), OutfitCreationActivity.class);
-                    startActivity(intentWardrobe);
-                    finish();
-                    break;*/
-
-               /* case R.id.addItem:
-
-               /* case R.id.addItem:
-
-                    Intent intentAdd = new Intent(getApplicationContext(), clothingFront.class);
-                    startActivity(intentAdd);
-                    finish();
-                    break;
-                case R.id.contactSupport:
-                    Intent intentContact = new Intent(getApplicationContext(), ContactForm.class);
-                    startActivity(intentContact);
-                    finish();
-                    break; */
-
-        binding.bottomNavigationView.setOnItemSelectedListener(new BottomNavigationView.OnItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                switch (item.getItemId()) {
-                    case R.id.home:
-                        Intent intentHome = new Intent(getApplicationContext(), OutfitCreationActivity.class);
-                        startActivity(intentHome);
-                        finish();
-                    case R.id.addItem:
-                        Intent intentAdd = new Intent(getApplicationContext(), clothingFront.class);
-                        startActivity(intentAdd);
-                        finish();
-                        break;
-                    case R.id.contactSupport:
-                        return true;
-                    case R.id.wardrobe:
-                        Intent intentWardrobe = new Intent(getApplicationContext(), AllSavedOufitsPage.class);
-                        startActivity(intentWardrobe);
-                        finish();
-                        break;
-                    /*case R.id.settings:
-                        startActivity(new Intent(OutfitCreationActivity.this, SETTINGS.class));
-                        break;*/
-                }
-                return true;
-            }
-        });
-        //end nav bar
-
 
         setContentView(R.layout.activity_contact_form);
 
@@ -132,7 +76,50 @@ public class ContactForm extends AppCompatActivity {
 
                 }
             });
-        }
+
+        //navigation buttons
+        binding.homeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), OutfitCreationActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+        binding.wardrobeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), AllSavedOufitsPage.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+        binding.addItemButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), clothingFront.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+        binding.contactButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), AboutUsPageActivity.class); //change depending on romans pages
+                startActivity(intent);
+                finish();
+            }
+        });
+        /*binding.settingsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), ?.class);
+                startActivity(intent);
+                finish();
+            }
+        });*/
+        //end nav buttons
+    }//end oncreate
 
     public void openSuccessPage(){
         Intent intent = new Intent(this, SuccessPage.class);
