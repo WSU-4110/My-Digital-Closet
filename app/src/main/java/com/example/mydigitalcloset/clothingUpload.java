7dc6df477626b42a9637b1cb67849a5b3078f103
Katie O'Donnell <122855315+k-odonnell@users.noreply.github.com>
2023-03-30 17:10:00 -0400

    added nav button functionality to clothing upload

--- app/src/main/java/com/example/mydigitalcloset/clothingUpload.java
+++ app/src/main/java/com/example/mydigitalcloset/clothingUpload.java
@@ -86,5 +86,48 @@ public class clothingUpload extends AppCompatActivity {
                 mDatabase.child("other").child(clothingId).setValue("new item");
             }
         });
-    }
+
+        //navigation buttons
+        binding.homeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), OutfitCreationActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+        binding.wardrobeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), AllSavedOufitsPage.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+        binding.addItemButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), clothingFront.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+        binding.contactButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), AboutUsPageActivity.class); //change depending on romans pages
+                startActivity(intent);
+                finish();
+            }
+        });
+        /*binding.settingsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), ?.class);
+                startActivity(intent);
+                finish();
+            }
+        });*/
+        //end nav buttons
+    }//end oncreate
 }
