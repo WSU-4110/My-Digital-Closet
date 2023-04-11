28970f576ed3eba9b444ef8f72561bf38b6353c8
Ayesha Helal <ayeshahelal11@gmail.com>
2023-04-11 15:26:59 -0400

    Recreated Saved Outfits Page

---
+++ app/src/main/java/com/example/mydigitalcloset/closetPage/AllSavedOutfits.java
@@ -0,0 +1,18 @@
+package com.example.mydigitalcloset.closetPage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.mydigitalcloset.R;
+
+public class AllSavedOutfits extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_all_saved_outfits);
+    }
+
+
+}
