20742a731552e694694e73cbc05d47ea0e8aa57b
Katie O'Donnell <122855315+k-odonnell@users.noreply.github.com>
2023-04-13 17:07:25 -0400

    added code to populate outfit listview with outfits in database

--- app/src/main/java/com/example/mydigitalcloset/closetPage/AllSavedOutfits.java
+++ app/src/main/java/com/example/mydigitalcloset/closetPage/AllSavedOutfits.java
@@ -1,27 +1,46 @@
 package com.example.mydigitalcloset.closetPage;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.ListView;
 
 import com.example.mydigitalcloset.AboutUsPageActivity;
 import com.example.mydigitalcloset.ContactForm;
+import com.example.mydigitalcloset.Outfit;
 import com.example.mydigitalcloset.OutfitCreationActivity;
 import com.example.mydigitalcloset.R;
 import com.example.mydigitalcloset.SettingsPage;
 import com.example.mydigitalcloset.clothingFront;
 import com.example.mydigitalcloset.databinding.ActivityAllSavedOutfitsBinding;
 import com.example.mydigitalcloset.databinding.ActivityOutfitCreationBinding;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
 
 public class AllSavedOutfits extends AppCompatActivity {
     private ImageView rectangle_1;
     ActivityAllSavedOutfitsBinding binding;
 
     Context context;
+
+    final FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference ref = database.getReference();
+    DatabaseReference outfitsRef = ref.child("outfits");
+    ListView listView;
+    ArrayList<String> arrayList = new ArrayList<>();
+    ArrayAdapter<String> arrayAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,13 +49,47 @@ public class AllSavedOutfits extends AppCompatActivity {
         binding = ActivityAllSavedOutfitsBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        listView = (ListView) findViewById(R.id.outfitList);
+        arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, arrayList);
+        listView.setAdapter(arrayAdapter);
+        outfitsRef.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                String value = snapshot.getValue(Outfit.class).toString();
+                arrayList.add(value);
+                arrayAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot snapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+
+        /*
         rectangle_1 = (ImageView) findViewById(R.id.rectangle_1);
         rectangle_1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
             openOutfit();
         }
-        });
+        });*/
 
         //navigation buttons
         binding.homeButton.setOnClickListener(new View.OnClickListener() {
