d13d35f6f6e35d9bf93016a31c1228a8de70686a
Katie O'Donnell <122855315+k-odonnell@users.noreply.github.com>
2023-04-13 15:25:59 -0400

    added nav bar functionality to all saved outfits page

--- app/src/main/java/com/example/mydigitalcloset/closetPage/AllSavedOutfits.java
+++ app/src/main/java/com/example/mydigitalcloset/closetPage/AllSavedOutfits.java
@@ -2,66 +2,85 @@ package com.example.mydigitalcloset.closetPage;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 
+import com.example.mydigitalcloset.AboutUsPageActivity;
 import com.example.mydigitalcloset.ContactForm;
 import com.example.mydigitalcloset.OutfitCreationActivity;
 import com.example.mydigitalcloset.R;
+import com.example.mydigitalcloset.SettingsPage;
 import com.example.mydigitalcloset.clothingFront;
 import com.example.mydigitalcloset.databinding.ActivityAllSavedOutfitsBinding;
+import com.example.mydigitalcloset.databinding.ActivityOutfitCreationBinding;
 
 public class AllSavedOutfits extends AppCompatActivity {
     private ImageView rectangle_1;
     ActivityAllSavedOutfitsBinding binding;
+
+    Context context;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        //NAV BAR STUFF:
+        // Inflate the layout
         binding = ActivityAllSavedOutfitsBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
-            switch(item.getItemId()){
-                case R.id.home:
-                    Intent intentHome = new Intent(this, OutfitCreationActivity.class);
-                    startActivity(intentHome);
-                    finish();
-                    break;
-                /*case R.id.wardrobe:
-                    Intent intentWardrobe = new Intent(getApplicationContext(), OutfitCreationActivity.class);
-                    startActivity(intentWardrobe);
-                    finish();
-                    break;*/
-                case R.id.addItem:
-                    Intent intentAdd = new Intent(this, clothingFront.class);
-                    startActivity(intentAdd);
-                    finish();
-                    break;
-                case R.id.contactSupport:
-                    Intent intentContact = new Intent(this, ContactForm.class);
-                    startActivity(intentContact);
-                    finish();
-                    break;
-            }
-
-            return true;
-        });
-        //end nav bar
-
-        setContentView(R.layout.activity_all_saved_outfits);
-
         rectangle_1 = (ImageView) findViewById(R.id.rectangle_1);
         rectangle_1.setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View view) {
+            @Override
+            public void onClick(View view) {
             openOutfit();
         }
-    });
-}
+        });
+
+        //navigation buttons
+        binding.homeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), OutfitCreationActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+        binding.wardrobeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),  AllSavedOutfits.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+        binding.addItemButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), clothingFront.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+        binding.contactButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), AboutUsPageActivity.class); //change depending on romans pages
+                startActivity(intent);
+                finish();
+            }
+        });
+        binding.settingsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), SettingsPage.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+    }
 
     public void openOutfit(){
         Intent intent = new Intent(this, SavedFit.class);
