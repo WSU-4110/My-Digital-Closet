e2a94e96dd4d1db7cbd4377bfc694d77bbfed91a
Katie O'Donnell <122855315+k-odonnell@users.noreply.github.com>
2023-04-17 15:02:24 -0400

    changed "get item" lines so user can get items saved w/o .png suffix

--- app/src/main/java/com/example/mydigitalcloset/OutfitCreationActivity.java
+++ app/src/main/java/com/example/mydigitalcloset/OutfitCreationActivity.java
@@ -291,7 +291,7 @@ public class OutfitCreationActivity extends AppCompatActivity {
                         progressDialog.setMessage("Fetching bottoms image...");
                         progressDialog.setCancelable(false);
                         progressDialog.show();
-                        storageReference = FirebaseStorage.getInstance().getReference("images/bottoms/"+bottomsID+".png");
+                        storageReference = FirebaseStorage.getInstance().getReference("images/bottoms/"+bottomsID);
                         //create local file for top image
                         try{
                             File bottomsfile = File.createTempFile("tempfile_bottoms", ".png");
@@ -345,7 +345,7 @@ public class OutfitCreationActivity extends AppCompatActivity {
                         progressDialog.setMessage("Fetching shoes image...");
                         progressDialog.setCancelable(false);
                         progressDialog.show();
-                        storageReference = FirebaseStorage.getInstance().getReference("images/shoes/"+shoesID+".png");
+                        storageReference = FirebaseStorage.getInstance().getReference("images/shoes/"+shoesID);
                         //create local file for shoes image
                         try{
                             File shoesfile = File.createTempFile("tempfile_shoes", ".png");
@@ -398,7 +398,7 @@ public class OutfitCreationActivity extends AppCompatActivity {
                         progressDialog.setMessage("Fetching headwear image...");
                         progressDialog.setCancelable(false);
                         progressDialog.show();
-                        storageReference = FirebaseStorage.getInstance().getReference("images/headwear/"+headwearID+".png");
+                        storageReference = FirebaseStorage.getInstance().getReference("images/headwear/"+headwearID);
                         //create local file for headwear image
                         try{
                             File headwearfile = File.createTempFile("tempfile_headwear", ".png");
@@ -451,7 +451,7 @@ public class OutfitCreationActivity extends AppCompatActivity {
                         progressDialog.setMessage("Fetching socks image...");
                         progressDialog.setCancelable(false);
                         progressDialog.show();
-                        storageReference = FirebaseStorage.getInstance().getReference("images/socks/"+socksID+".png");
+                        storageReference = FirebaseStorage.getInstance().getReference("images/socks/"+socksID);
                         //create local file for socks image
                         try{
                             File socksfile = File.createTempFile("tempfile_socks", ".png");
@@ -504,7 +504,7 @@ public class OutfitCreationActivity extends AppCompatActivity {
                         progressDialog.setMessage("Fetching other image...");
                         progressDialog.setCancelable(false);
                         progressDialog.show();
-                        storageReference = FirebaseStorage.getInstance().getReference("images/other/"+otherID+".png");
+                        storageReference = FirebaseStorage.getInstance().getReference("images/other/"+otherID);
                         //create local file for other image
                         try{
                             File otherfile = File.createTempFile("tempfile_other", ".png");
