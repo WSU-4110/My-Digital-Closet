a06e39b212abd6a2347979bc7689b496f8474c7e
David <awsone115@gmail.com>
2023-04-17 23:42:21 -0400

    Created a system for sending and verifying 2FA codes via email using Firebase Cloud Functions and Nodemailer. Configured secure Gmail API access for sending emails without issues related to 2FA on the Gmail account.

--- app/src/main/java/com/example/mydigitalcloset/Login.java
+++ app/src/main/java/com/example/mydigitalcloset/Login.java
@@ -1,11 +1,6 @@
 package com.example.mydigitalcloset;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
@@ -13,41 +8,40 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.functions.FirebaseFunctions;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class Login extends AppCompatActivity {
 
-    private TextInputEditText textInputEditTextPassword, textInputEditTextUsername;
-    private Button buttonLogin;
+    private TextInputEditText textInputEditTextPassword, textInputEditTextUsername, textInputEditTextVerificationCode;
+    private Button buttonLogin, buttonSendCode;
     private TextView textViewSignUp;
     private FirebaseAuth mAuth;
-    private SharedPreferences sharedPreferences;
+    private FirebaseFunctions mFunctions;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
 
         mAuth = FirebaseAuth.getInstance();
-        sharedPreferences = getSharedPreferences("MyDigitalClosetPrefs", Context.MODE_PRIVATE);
-
-        // Check if the user is already logged in and if they just logged in
-        boolean userJustLoggedIn = sharedPreferences.getBoolean("userJustLoggedIn", false);
-        if (mAuth.getCurrentUser() != null && userJustLoggedIn) {
-            // If the user is logged in and just logged in, navigate to the OutfitCreationActivity
-            Intent intent = new Intent(getApplicationContext(), OutfitCreationActivity.class);
-            startActivity(intent);
-            finish();
-        }
-
-        setContentView(R.layout.activity_login);
+        mFunctions = FirebaseFunctions.getInstance();
 
         textInputEditTextUsername = findViewById(R.id.username);
         textInputEditTextPassword = findViewById(R.id.password);
+        textInputEditTextVerificationCode = findViewById(R.id.verification_code);
         buttonLogin = findViewById(R.id.buttonLogin);
+        buttonSendCode = findViewById(R.id.buttonSendCode);
         textViewSignUp = findViewById(R.id.signUpText);
 
         textViewSignUp.setOnClickListener(new View.OnClickListener() {
@@ -58,39 +52,95 @@ public class Login extends AppCompatActivity {
                 finish();
             }
         });
+
+        buttonSendCode.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String username = String.valueOf(textInputEditTextUsername.getText());
+
+                if (!TextUtils.isEmpty(username)) {
+                    sendVerificationCode(username)
+                            .addOnCompleteListener(new OnCompleteListener<String>() {
+                                @Override
+                                public void onComplete(@NonNull Task<String> task) {
+                                    if (task.isSuccessful()) {
+                                        Toast.makeText(Login.this, "Verification code sent!", Toast.LENGTH_SHORT).show();
+                                    } else {
+                                        Toast.makeText(Login.this, "Failed to send verification code.", Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            });
+                } else {
+                    Toast.makeText(getApplicationContext(), "Please enter your email address", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
         buttonLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String username, password;
+                String username, password, code;
                 username = String.valueOf(textInputEditTextUsername.getText());
                 password = String.valueOf(textInputEditTextPassword.getText());
+                code = String.valueOf(textInputEditTextVerificationCode.getText());
 
-                if (!TextUtils.isEmpty(username) && !TextUtils.isEmpty(password)) {
+                if (!TextUtils.isEmpty(username) && !TextUtils.isEmpty(password) && !TextUtils.isEmpty(code)) {
                     mAuth.signInWithEmailAndPassword(username, password)
                             .addOnCompleteListener(Login.this, new OnCompleteListener<AuthResult>() {
                                 @Override
                                 public void onComplete(@NonNull Task<AuthResult> task) {
                                     if (task.isSuccessful()) {
-                                        Toast.makeText(Login.this, "Login successful!", Toast.LENGTH_SHORT).show();
-                                        sharedPreferences.edit().putBoolean("userJustLoggedIn", true).apply();
-                                        Intent intent = new Intent(getApplicationContext(), OutfitCreationActivity.class);
-                                        startActivity(intent);
-                                        finish();
+                                        verifyCode(username, code)
+                                                .addOnCompleteListener(new OnCompleteListener<String>() {
+                                                    @Override
+                                                    public void onComplete(@NonNull Task<String> task) {
+                                                        if (task.isSuccessful()) {
+                                                            Toast.makeText(Login.this, "Login successful!", Toast.LENGTH_SHORT).show();
+                                                            Intent intent = new Intent(getApplicationContext(), OutfitCreationActivity.class);
+                                                            startActivity(intent);
+                                                            finish();
+                                                        } else {
+                                                            Toast.makeText(Login.this, "Invalid or expired verification code.", Toast.LENGTH_SHORT).show();
+                                                        }
+                                                    }
+                                                });
                                     } else {
-                                        Toast.makeText(Login.this, "Authentication failed.", Toast.LENGTH_SHORT).show();
+                                        Toast.makeText(Login.this, "Authentication failed.",
+                                                Toast.LENGTH_SHORT).show();
                                     }
                                 }
-                            });
-                } else {
-                    Toast.makeText(getApplicationContext(), "All fields required", Toast.LENGTH_SHORT).show();
+                            });                 } else {
+                    Toast.makeText(getApplicationContext(), "All fields are required", Toast.LENGTH_SHORT).show();
                 }
             }
         });
     }
 
-    @Override
-    protected void onPause() {
-        super.onPause();
-        sharedPreferences.edit().putBoolean("userJustLoggedIn", false).apply();
+    private Task<String> sendVerificationCode(String email) {
+        Map<String, Object> data = new HashMap<>();
+        data.put("email", email);
+
+        return mFunctions
+                .getHttpsCallable("sendVerificationCode")
+                .call(data)
+                .continueWith(task -> {
+                    String result = (String) task.getResult().getData();
+                    return result;
+                });
+    }
+
+    private Task<String> verifyCode(String email, String code) {
+        Map<String, Object> data = new HashMap<>();
+        data.put("email", email);
+        data.put("code", code);
+
+        return mFunctions
+                .getHttpsCallable("verifyCode")
+                .call(data)
+                .continueWith(task -> {
+                    String result = (String) task.getResult().getData();
+                    return result;
+                });
     }
 }
+
--- app/src/test/java/com/example/mydigitalcloset/ExampleUnitTest.java
+++ app/src/test/java/com/example/mydigitalcloset/ExampleUnitTest.java
@@ -1,14 +1,8 @@
 package com.example.mydigitalcloset;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
-import static org.junit.Assert.*;
-
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
- */
 public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() {
